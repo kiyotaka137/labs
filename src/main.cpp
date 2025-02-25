cbd020f48388fa2c0df656bb07775fdc92e27f15
kiyotaka137 <rsainov@yandex.ru>
2025-02-25 18:51:49 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include"utils.cpp"
+#include<iostream>
+int main() {
+	int a = 1;
+	int b = 3;
+	std::cout << summ(a, b) << std::endl;
+	std::cout << sub(a, b);
+	return 0;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,6 @@
+inline int summ(int a, int b) {
+	return a + b;
+}
+inline int sub(int a, int b) {
+	return a - b;
+}
