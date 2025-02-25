9c6d2e3788cbabb129615611e8b03abcb20cf513
kiyotaka137 <rsainov@yandex.ru>
2025-02-25 18:56:26 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ int main() {
 	int a = 1;
 	int b = 3;
 	std::cout << summ(a, b) << std::endl;
-	std::cout << sub(a, b);
+	std::cout << sub(a, b) << std::endl;
+	std::cout << multiply(a, b);
 	return 0;
 }
